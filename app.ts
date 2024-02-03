e6888ead9193ffd386e80133feb030cdb5f214c6
Dylan Vu <dylanvu9@gmail.com>
2024-02-02 21:31:07 -0800

    initialize websockets

--- app.ts
+++ app.ts
@@ -1,12 +1,26 @@
 import express from "express";
+import http from "http";
+import { WebSocketServer } from "ws";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
+app.use(cors());
+app.use(express.json());
+
+// create the
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({ server: server });
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
+wss.on("connection", (socket) => {
+    console.log("A user has connected!");
+})
+
+server.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
